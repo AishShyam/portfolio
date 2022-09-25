694a0621f86263a4148e64f735d12e8ebaefefda
Aishwarya CS <aishwarya.shyam5@gmail.com>
2022-09-25 14:57:20 +0530

    updating resume, adding a few css changes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,30 @@
 import programmerLogo from '../asset/programmer.png'
 import downloadLogo from '../asset/download.png'
-import AishwaryaCS from "../document/AishwaryaCS.pdf"
+import AishwaryaCS from "../document/AishwaryaCS_resume.pdf"
 
 
 function Hero() {
     return (
         <>
             <div className='hero'>
-                <img src={programmerLogo} width="100px"/>
-                <h2 className='hero--name'>Hey there!</h2>
-                <h2 className='hero--name'>I'm Aishwarya</h2>
-                <div className="i-title">
-                    <div className="i-title-wrapper">
-                        <div className="i-title-item"><h2>Web Developer</h2></div>
-                        <div className="i-title-item"><h2>Web3 Developer</h2></div>
-                        <div className="i-title-item"><h2>Pretentious Writer</h2></div>
-                        <div className="i-title-item"><h2>Wannabe Movie Critic</h2></div>
-                        <div className="i-title-item"><h2>Learner</h2></div>
+                <div className='hero--intro'>
+                    <img src={programmerLogo} width="100px"/>
+                    <h2 className='hero--name'>Hey there!</h2>
+                    <h2 className='hero--name'>I'm Aishwarya</h2>
+                    <div className="i-title">
+                        <div className="i-title-wrapper">
+                            <div className="i-title-item"><h2>Developer</h2></div>
+                            <div className="i-title-item"><h2>Web Developer</h2></div>
+                            <div className="i-title-item"><h2>Blockchain Enthusiast</h2></div>
+                            <div className="i-title-item"><h2>Learner :)</h2></div>
+                        </div>
                     </div>
                 </div>
                 <div className='download--resume'>                
                     <a href={AishwaryaCS} download="AishwaryaCS" target='_blank'>
                         <img src={downloadLogo} width="40px"/>
                     </a>
-                    <div className='downloadresume'><p>DownloadResume</p></div>                             
+                    <div className='downloadresume'><p>Download Resume</p></div>                             
                 </div>
             </div>
         </>
